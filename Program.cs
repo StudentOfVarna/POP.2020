cf1432723fd38a3556d8710faab35924d501b946
StudentOfVarna <gatien.fournier@edu.devinci.fr>
2020-10-05 20:01:19 +0300

    zt

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace POP._2020
     {
         static void Main(string[] args)
         {
+            MeAfterTen();
+        }
+
+        static void MeAfterTen()
+        {
+            Console.WriteLine("What is your age ?");
+            string ageAsString = Console.ReadLine();
+            int age;
+            age = int.Parse(ageAsString);
+            //age = Convert.ToInt32(ageAsString);
+            int ageten = age + 10;
+            Console.WriteLine("Your age in 10 years will be " + ageten);
+            Console.ReadKey();
         }
     }
 }
